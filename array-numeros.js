f2563105507282f99d79deed0e06ae326c20e56d
practicodepc <practicode@hotmail.com>
2022-01-08 15:55:15 -0600

    f

--- array-numeros.js
+++ array-numeros.js
@@ -1,13 +1,46 @@
 let numeros = [1, 2, 3];
 
-let numerosMultiplicados = numeros.map(function(item, index, array){
+let resultado = numeros.map(function(item, index, array){
     return item * 2;
 })
 
-// let numerosMultiplicados = numeros.map(function(item){
-//     return item * 2;
-// })
+console.log(resultado);
 
-// let numerosMultiplicados = numeros.map(item => item * 2);
-// console.log(numerosMultiplicados);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+    let numerosMultiplicados = numeros.map(function(item){
+        return item * 2;
+    })
+
+
+    let numerosMultiplicados = numeros.map(item => item * 2);
+    console.log(numerosMultiplicados);
+
+
+    let numerosMultiplicados = [1, 2, 3].map(item => item * 2);
+
+
+    let numeros = [1, 2, 3];
+
+    let funcionMap = function(item){
+        return item * 2;
+    }
+    
+    let numerosMultiplicados = numeros.map(funcionMap);
+ */
+
+
 
