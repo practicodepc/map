144250f0b4db452a79b2e286160aa39b7da9e6de
practicodepc <practicode@hotmail.com>
2022-01-08 12:10:34 -0600

    f

---
+++ array-numeros.js
@@ -0,0 +1,13 @@
+let numeros = [1, 2, 3];
+
+let numerosMultiplicados = numeros.map(function(item, index, array){
+    return item * 2;
+})
+
+// let numerosMultiplicados = numeros.map(function(item){
+//     return item * 2;
+// })
+
+// let numerosMultiplicados = numeros.map(item => item * 2);
+// console.log(numerosMultiplicados);
+
---
+++ array-objetos.js
@@ -0,0 +1,53 @@
+let personas = [
+    {nombre: "Maria", apellido: "Reyes"},
+    {nombre: "Pedro", apellido: "Sanchez"},
+    {nombre: "Juan", apellido: "Perez"},
+];
+
+let personasModificado = personas.map(function(item, index, array) {
+    let objetoNuevo = {};
+    objetoNuevo["nombreCompleto"] = item.nombre + " " + item.apellido; //`${item.nombre} ${item.apellido}`
+    return objetoNuevo;
+})
+
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+console.log(personasModificado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
